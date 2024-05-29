a0f338beaa261f7a6f48a7d7084c68dd835f3be1
Maurice Skinner <skinnermes@gmail.com>
2024-05-29 23:39:41 +0200

    added and changed props

--- src/main/java/com/mooc/kubernetes/NoteEntity.java
+++ src/main/java/com/mooc/kubernetes/NoteEntity.java
@@ -18,7 +18,7 @@ public class NoteEntity {
 
   @jakarta.persistence.Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Integer id;
+  private Long id;
   private String note;
   private Boolean isDone;
 
